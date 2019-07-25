04ee8168de0c0534158c4ae27010778d9d401560
Neil Aitken <n.aitken@gmail.com>
2019-07-25 22:20:28 +0100

    Laser bouncing

--- scenes/Emitters/Laser.gd
+++ scenes/Emitters/Laser.gd
@@ -2,14 +2,31 @@ extends Node2D
 
 var cast_length = 500
 
+onready var active_beams = [
+	$FirstBeam
+]
+
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	if $FirstBeam.is_colliding():
-		var collider = $FirstBeam.get_collider()
-		if collider is BaseMirror:
-			bounce_beam(collider, $FirstBeam.get_collision_point())
+	for index in range(active_beams.size()):
+		var beam = active_beams[index]
+		if beam and beam.is_colliding():
+			
+			var collider = beam.get_collider()
+			
+			if collider is BaseMirror:
+				active_beams.remove(index)
+				var line = Line2D.new()
+				line.add_point(beam.global_position)
+				line.add_point(beam.get_collision_point())
+				line.width = 2
+				line.default_color = Color.red
+				get_node("../").add_child(line)
+				bounce_beam(collider, beam.get_collision_point())
+		else:
+			break
 
 func bounce_beam(mirror : BaseMirror, collision_point : Vector2):
 	var bounce_dir = mirror.get_bounce_dir()
@@ -19,4 +36,8 @@ func bounce_beam(mirror : BaseMirror, collision_point : Vector2):
 	cast.collide_with_areas = true
 	cast.enabled = true
 	cast.global_position = collision_point - global_position
+	cast.add_exception(mirror)
+
+	active_beams.append(cast)
+
 	add_child(cast)
