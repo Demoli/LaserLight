1d290f2cd4fbe6ffddfc5b7c90bfd69655c861ab
Neil Aitken <n.aitken@gmail.com>
2019-07-28 13:44:11 +0100

    Add beam splitters

--- scenes/Emitters/Laser.gd
+++ scenes/Emitters/Laser.gd
@@ -14,6 +14,9 @@ func _ready():
 
 func _process(delta):
 	for index in range(active_beams.size()):
+		if index > active_beams.size() -1 :
+			break
+			
 		var beam = active_beams[index]
 		if beam and beam.is_colliding():
 			
@@ -24,8 +27,8 @@ func _process(delta):
 			
 			if collider is BaseMirror:
 				bounce_beam(collider, beam.get_collision_point(), beam)
-#			if collider is Splitter:
-#				split_beam(collider, beam.get_collision_point(), beam)
+			if collider is Splitter:
+				split_beam(collider, beam.get_collision_point(), beam)
 			if collider is BaseTarget and collider.color == color:
 				print('beam reached targeet ' + collider.name)
 			if collider is LevelTilemap or collider is BaseObstacle:
@@ -38,20 +41,16 @@ func bounce_beam(mirror : BaseMirror, collision_point : Vector2, beam : RayCast2
 	var cast_direction = collision_direction.rotated(deg2rad(mirror.get_entry_bounce_dir(shape)))
 	var beamdir = mirror.position.direction_to(beam.position)
 	
-#	if mirror.global_position.x < global_position.x:
-#		cast_direction *= -1
-
 	_create_beam(mirror.global_position - global_position, cast_direction, mirror)
 
-func split_beam(mirror : BaseMirror, collision_point : Vector2, beam : RayCast2D):
-	var collision_direction = to_global(beam.position).direction_to(collision_point)
-	var cast_direction = collision_direction.rotated(deg2rad(45))
+func split_beam(mirror : Splitter, collision_point : Vector2, beam : RayCast2D):
 	
-	if mirror.global_position.x < beam.global_position.x or beam.global_position.y < mirror.global_position.y:
-		cast_direction *= -1
-
-	_create_beam(collision_point - global_position, cast_direction, mirror)
-
+	var angles = [45,-45]
+	var collision_direction = to_global(beam.position).direction_to(collision_point)
+		
+	for angle in angles:
+		var cast_direction = collision_direction.rotated(deg2rad(angle))
+		_create_beam(collision_point - global_position, cast_direction, mirror)
 
 func _create_beam(initial_position : Vector2, cast_direction : Vector2, exception):
 	var cast = RayCast2D.new()
