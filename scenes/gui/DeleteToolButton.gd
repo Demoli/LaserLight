b81ddea19ef69fdbfc5a7f0a7bd7d70fb0354652
Neil Aitken <n.aitken@gmail.com>
2019-07-28 16:01:15 +0100

    Create temporary del;ete area

--- scenes/gui/DeleteToolButton.gd
+++ scenes/gui/DeleteToolButton.gd
@@ -1,17 +1,6 @@
-extends TextureButton
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+extends Area2D
 
 
-func _on_DeleteToolButton_focus_entered():
-	pass # Replace with function body.
+
+func _on_DeleteToolButton_area_shape_entered(area_id, area, area_shape, self_shape):
+	area.queue_free()
