056ac13d16d7cb286a6f38f6621e43a0f07bc57d
Neil Aitken <n.aitken@gmail.com>
2019-07-28 13:22:55 +0100

    Fix the collisions by using two entry points

--- scenes/Emitters/Laser.gd
+++ scenes/Emitters/Laser.gd
@@ -24,6 +24,8 @@ func _process(delta):
 			
 			if collider is BaseMirror:
 				bounce_beam(collider, beam.get_collision_point(), beam)
+#			if collider is Splitter:
+#				split_beam(collider, beam.get_collision_point(), beam)
 			if collider is BaseTarget and collider.color == color:
 				print('beam reached targeet ' + collider.name)
 			if collider is LevelTilemap or collider is BaseObstacle:
@@ -32,22 +34,37 @@ func _process(delta):
 
 func bounce_beam(mirror : BaseMirror, collision_point : Vector2, beam : RayCast2D):
 	var collision_direction = to_global(beam.position).direction_to(collision_point)
-	var cast_direction = collision_direction.rotated(deg2rad(mirror.bounce_dir))
+	var shape = mirror.get_collision_shape(beam.get_collider_shape())
+	var cast_direction = collision_direction.rotated(deg2rad(mirror.get_entry_bounce_dir(shape)))
+	var beamdir = mirror.position.direction_to(beam.position)
 	
-	if mirror.global_position.x < global_position.x:
+#	if mirror.global_position.x < global_position.x:
+#		cast_direction *= -1
+
+	_create_beam(mirror.global_position - global_position, cast_direction, mirror)
+
+func split_beam(mirror : BaseMirror, collision_point : Vector2, beam : RayCast2D):
+	var collision_direction = to_global(beam.position).direction_to(collision_point)
+	var cast_direction = collision_direction.rotated(deg2rad(45))
+	
+	if mirror.global_position.x < beam.global_position.x or beam.global_position.y < mirror.global_position.y:
 		cast_direction *= -1
 
+	_create_beam(collision_point - global_position, cast_direction, mirror)
+
+
+func _create_beam(initial_position : Vector2, cast_direction : Vector2, exception):
 	var cast = RayCast2D.new()
 	cast.add_to_group("beam_casts")
 	cast.cast_to = Vector2(cast_length, cast_length) * cast_direction
 	cast.collide_with_areas = true
 	cast.enabled = true
-	cast.global_position = collision_point - global_position
-	cast.add_exception(mirror)
+	cast.global_position = initial_position
+	cast.add_exception(exception)
 
 	active_beams.append(cast)
 
-	add_child(cast)
+	add_child(cast)	
 
 func _draw_beam(beam : RayCast2D, target):
 	var line = Line2D.new()
--- scenes/Mirrors/BaseMirror.gd
+++ scenes/Mirrors/BaseMirror.gd
@@ -3,6 +3,21 @@ extends Area2D
 class_name BaseMirror
 
 var bounce_dir = 0 setget ,get_bounce_dir
+var collision_shapes = []
+
+func _ready():
+	for child in get_children():
+		if child is CollisionShape2D:
+			collision_shapes.append(child)
 
 func get_bounce_dir() -> float:
 	return bounce_dir
+	
+func get_collision_shape(id: int) -> CollisionShape2D:
+	return collision_shapes[id]
+
+func get_entry_bounce_dir(shape : CollisionShape2D):
+	if shape.name == 'FacingEntry':
+		return bounce_dir
+	if shape.name == 'InvertedEntry':
+		return bounce_dir * -1
